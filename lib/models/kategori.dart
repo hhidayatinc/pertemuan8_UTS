e7c2fb643507b611d7c1de7761eebf7daa3d065b
hhidayatinc <chasanah55@gmail.com>
2021-03-31 14:50:46 +0700

    pembuatan models

---
+++ lib/models/kategori.dart
@@ -0,0 +1,10 @@
+class Kategori {
+  int _id;
+  String _title;
+
+  get id => this._id;
+  set id(value) => this._id = value;
+  
+  String get title => this._title;
+  set title(String value) => this._title = value;
+}
